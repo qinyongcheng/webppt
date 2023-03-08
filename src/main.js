5ae481e994a635cb0b86c7dc192e317b4d2cbe5d
qyc <girg@163.com>
2023-03-08 22:35:09 +0800

    cdn引入
    Signed-off-by: qyc <girg@163.com>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 //https://blog.csdn.net/qq_34461600/article/details/126048096
+//http://www.studyofnet.com/741566914.html
 createApp(App).mount('#app')
